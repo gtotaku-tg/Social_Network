ab89476bc4af7903e7e905d76cf39c3b66d0b2d6
gtotaku-tg <124806960+gtotaku-tg@users.noreply.github.com>
2023-08-15 14:47:30 -0400

    all files fixed for nameing

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-
+router.use((req, res) => res.send('Not this route!'));
 
 
 module.exports = router;
